c2b1c683995b9a3bb0087a16dca271ad081f8bb3
Alexey <alexey@alexey>
2024-04-05 16:33:43 +0300

    publish photo to Telegram channel

--- post_space_image_bot.py
+++ post_space_image_bot.py
@@ -6,4 +6,4 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
     load_dotenv()
     bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
-    bot.send_message(chat_id=os.environ["CHAT_ID"], text="I'm sorry Dave I'm afraid I can't do that.")
+    bot.send_document(chat_id=os.environ["CHAT_ID"], document=open('images/hubble.jpeg', 'rb'))
