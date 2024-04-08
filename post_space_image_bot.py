3651b0ab1754b071fee20c8c6a10be2c6f73546f
Alexey <alexey@alexey>
2024-04-08 13:48:58 +0300

    deletes post_space_image_bot.py

--- post_space_image_bot.py
+++
@@ -1,9 +0,0 @@
-import os
-import telegram
-from dotenv import load_dotenv
-
-
-if __name__ == '__main__':
-    load_dotenv()
-    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
-    bot.send_document(chat_id=os.environ["CHAT_ID"], document=open('images/hubble.jpeg', 'rb'))
